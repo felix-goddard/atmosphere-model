ce53eae206320f1072c714d5c14b4bea8688a307
Felix Goddard <Felix.Goddard@pg.canterbury.ac.nz>
2025-05-21 01:17:28 +1200

    Output net flux

--- src/mod_output.f90
+++ src/mod_output.f90
@@ -4,7 +4,7 @@ module mod_output
    use mod_config, only: config => main_config
    use mod_netcdf, only: netcdf_file, create_netcdf
    use mod_tiles, only: isd, ied, jsd, jed
-   use mod_fields, only: dp, pt, ud, vd, gz, ts
+   use mod_fields, only: dp, pt, ud, vd, gz, ts, net_flux
 
    implicit none
 
@@ -15,7 +15,7 @@ module mod_output
    type(netcdf_file) :: output_nc
    type(netcdf_file) :: restart_nc
 
-   integer(ik), parameter :: n_output_fields = 5 ! number of outputs
+   integer(ik), parameter :: n_output_fields = 6 ! number of outputs
 
    real(rk), allocatable :: output_field(:, :, :, :)
    real(rk), allocatable :: compensation(:, :, :, :)
@@ -30,31 +30,34 @@ module mod_output
    integer(ik), parameter :: U_IDX = 3
    integer(ik), parameter :: V_IDX = 4
    integer(ik), parameter :: TS_IDX = 5
+   integer(ik), parameter :: NFLX_IDX = 6
 
 contains
 
    subroutine init_output()
-      integer(ik) :: i
+      integer(ik) :: i, nlay
+
+      nlay = config%nlay
 
       ! Allocate output arrays
 
       if (.not. allocated(output_field)) &
-         allocate (output_field(isd:ied, jsd:jed, config%nlay, n_output_fields))
+         allocate (output_field(isd:ied, jsd:jed, nlay + 1, n_output_fields))
 
       if (.not. allocated(compensation)) &
-         allocate (compensation(isd:ied, jsd:jed, config%nlay, n_output_fields))
+         allocate (compensation(isd:ied, jsd:jed, nlay + 1, n_output_fields))
 
       output_field(:, :, :, :) = 0.
       compensation(:, :, :, :) = 0.
       accumulation_time = 0.
 
       if (.not. allocated(gather_coarray)) &
-         allocate (gather_coarray(config%nx, config%ny, config%nlay) [*])
+         allocate (gather_coarray(config%nx, config%ny, nlay + 1) [*])
 
       if (this_image() == 1) then
 
          if (.not. allocated(gather)) &
-            allocate (gather(config%nx, config%ny, config%nlay))
+            allocate (gather(config%nx, config%ny, nlay + 1))
 
          ! Create output netCDF
 
@@ -68,13 +71,15 @@ contains
          call output_nc%create_axis( &
             'y', [(-.5*config%Ly + (i + .5)*config%dy, i=0, config%ny - 1)])
 
-         call output_nc%create_axis('lay', [(i, i=1, config%nlay)])
+         call output_nc%create_axis('lay', [(i, i=1, nlay)])
+         call output_nc%create_axis('lev', [(i, i=1, nlay + 1)])
 
          call output_nc%create_variable('dp', ['t  ', 'x  ', 'y  ', 'lay'])
          call output_nc%create_variable('pt', ['t  ', 'x  ', 'y  ', 'lay'])
          call output_nc%create_variable('u', ['t  ', 'x  ', 'y  ', 'lay'])
          call output_nc%create_variable('v', ['t  ', 'x  ', 'y  ', 'lay'])
          call output_nc%create_variable('ts', ['t  ', 'x  ', 'y  '])
+         call output_nc%create_variable('nflx', ['t  ', 'x  ', 'y  ', 'lev'])
 
       end if
 
@@ -109,7 +114,14 @@ contains
                ! v wind
                call compensated_sum(dt*.5*(vd(i, j, k) + vd(i + 1, j, k)), &
                                     i, j, k, V_IDX)
+
+               ! net radiative flux
+               call compensated_sum(dt*net_flux(i, j, k), i, j, k, NFLX_IDX)
             end do
+
+            ! net radiative flux
+            call compensated_sum(dt*net_flux(i, j, config%nlay + 1), &
+                                 i, j, config%nlay + 1, NFLX_IDX)
          end do
       end do
 
@@ -131,25 +143,32 @@ contains
 
    subroutine write_output(time)
       real(rk), intent(in) :: time
+      integer(ik) :: nlay
+
+      nlay = config%nlay
 
       if (this_image() == 1) call output_nc%advance_time(time)
 
       output_field(:, :, :, :) = output_field(:, :, :, :)/accumulation_time
 
       ! Output pressure thickness field
-      call write_3d (output_nc, output_field(:, :, :, DP_IDX), 'dp')
+      call write_3d(output_nc, output_field(:, :, 1:nlay, DP_IDX), nlay, 'dp')
 
       ! Output potential temperature field
-      call write_3d (output_nc, output_field(:, :, :, PT_IDX), 'pt')
+      call write_3d(output_nc, output_field(:, :, 1:nlay, PT_IDX), nlay, 'pt')
 
       ! Output u wind
-      call write_3d (output_nc, output_field(:, :, :, U_IDX), 'u')
+      call write_3d(output_nc, output_field(:, :, 1:nlay, U_IDX), nlay, 'u')
 
       ! Output v wind
-      call write_3d (output_nc, output_field(:, :, :, V_IDX), 'v')
+      call write_3d(output_nc, output_field(:, :, 1:nlay, V_IDX), nlay, 'v')
 
       ! Output surface temperature
-      call write_2d (output_nc, output_field(:, :, 1, TS_IDX), 'ts')
+      call write_2d(output_nc, output_field(:, :, 1, TS_IDX), 'ts')
+
+      ! Output net radiative flux
+      call write_3d(output_nc, output_field(:, :, 1:nlay + 1, NFLX_IDX), &
+                    nlay + 1, 'nflx')
 
       output_field(:, :, :, :) = 0.
       compensation(:, :, :, :) = 0.
@@ -160,7 +179,9 @@ contains
    subroutine write_restart_file(filename, time)
       character(len=*), intent(in) :: filename
       real(rk), intent(in) :: time
-      integer(ik) :: i
+      integer(ik) :: i, nlay
+
+      nlay = config%nlay
 
       if (this_image() == 1) then
 
@@ -186,7 +207,7 @@ contains
             'yf', [(-.5*config%Ly + i*config%dy, i=0, config%ny - 1)])
 
          ! levels
-         call restart_nc%create_axis('lay', [(i, i=1, config%nlay)])
+         call restart_nc%create_axis('lay', [(i, i=1, nlay)])
 
          call restart_nc%create_variable('dp', ['xc ', 'yc ', 'lay'])
          call restart_nc%create_variable('pt', ['xc ', 'yc ', 'lay'])
@@ -197,33 +218,34 @@ contains
 
       end if
 
-      call write_3d (restart_nc, dp(isd:ied, jsd:jed, :), 'dp')
-      call write_3d (restart_nc, pt(isd:ied, jsd:jed, :), 'pt')
-      call write_3d (restart_nc, ud(isd:ied, jsd:jed, :), 'u')
-      call write_3d (restart_nc, vd(isd:ied, jsd:jed, :), 'v')
-      call write_2d (restart_nc, gz(isd:ied, jsd:jed, 1), 'gzs')
-      call write_2d (restart_nc, ts(isd:ied, jsd:jed), 'ts')
+      call write_3d(restart_nc, dp(isd:ied, jsd:jed, :), nlay, 'dp')
+      call write_3d(restart_nc, pt(isd:ied, jsd:jed, :), nlay, 'pt')
+      call write_3d(restart_nc, ud(isd:ied, jsd:jed, :), nlay, 'u')
+      call write_3d(restart_nc, vd(isd:ied, jsd:jed, :), nlay, 'v')
+      call write_2d(restart_nc, gz(isd:ied, jsd:jed, 1), 'gzs')
+      call write_2d(restart_nc, ts(isd:ied, jsd:jed), 'ts')
 
       if (this_image() == 1) call restart_nc%close()
 
    end subroutine write_restart_file
 
-   subroutine write_3d (netcdf, field, name)
+   subroutine write_3d(netcdf, field, k_max, name)
       type(netcdf_file), intent(inout) :: netcdf
-      real(rk), intent(in) :: field(isd:ied, jsd:jed, config%nlay)
+      real(rk), intent(in) :: field(isd:ied, jsd:jed, 1:k_max)
+      integer(ik) :: k_max
       character(len=*), intent(in) :: name
 
       sync all
-      gather_coarray(isd:ied, jsd:jed, :) [1] = field(:, :, :)
+      gather_coarray(isd:ied, jsd:jed, 1:k_max) [1] = field(:, :, 1:k_max)
       sync all
 
       if (this_image() == 1) then
-         gather(:, :, :) = gather_coarray(:, :, :)
-         call netcdf%write_variable(name, gather)
+         gather(:, :, 1:k_max) = gather_coarray(:, :, 1:k_max)
+         call netcdf%write_variable(name, gather(:, :, 1:k_max))
       end if
    end subroutine write_3d
 
-   subroutine write_2d (netcdf, field, name)
+   subroutine write_2d(netcdf, field, name)
       type(netcdf_file), intent(inout) :: netcdf
       real(rk), intent(in) :: field(isd:ied, jsd:jed)
       character(len=*), intent(in) :: name
